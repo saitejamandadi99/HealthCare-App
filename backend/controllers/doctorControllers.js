b616ad52186da6b9c7e26ea973366448535ca48a
saitejamandadi99 <saitejamandadi99>
2025-08-01 11:17:49 +0530

    completed doctors api for fetching all and individual details of doctors.

---
+++ backend/controllers/doctorControllers.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const doctors = require('../data/doctors');
+
+const getAllDoctorsList = (req, res) =>{
+    const previewDoctors = doctors.map(eachDoctor=>({
+        id:eachDoctor.id, 
+        name: eachDoctor.name, 
+        specialization:eachDoctor.specialization,
+    }));
+    try {
+        res.status(200).json({message:'Doctors list fetched successfully', doctorsList:previewDoctors}); //200 = success code
+    } catch (error) {
+        res.status(500).json({message: 'Internal Server Error'}); //500 = server error code
+    }
+}
+
+const getDoctorDetails = (req,res) =>{
+    const doctorId = req.params.id;
+    const doctor = doctors.find(eachDoctor => eachDoctor.id =doctorId);
+    try{
+        if(!doctor){
+            return res.status(404),json({message:'Doctor not found'}) //404 = not found code
+        }
+        res.status(200).json({message:'Doctors details fetched successfully', doctorDetails: doctor}); //200 = ok or success code
+    }
+    catch(error){
+        res.status(500).json({message: 'Internal Server Error'}); //500 = server error code
+    }
+}
+
