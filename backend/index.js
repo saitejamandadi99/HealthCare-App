bb8699e7defa1f75f44d5f4583ede6d16a1c8a7d
saitejamandadi99 <saitejamandadi99>
2025-08-01 12:38:55 +0530

    added routing and cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 app.use(express.json());
-
+app.use(cors());
 app.get('/', (req, res)=>{
     res.send('Welcome to the HealthCare App Api');
 })
 
+app.use('/api', require('./routers/doctorRoutes'));
+app.use('/api', require('./routers/appointmentRoutes'));
+
 app.listen(5000, ()=>{
     console.log('Server is running on the port http://localhost:5000');
 });
-
