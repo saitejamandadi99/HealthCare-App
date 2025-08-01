584553ee3927e2f16c9cb2e39d7ce35ac7a72601
saitejamandadi99 <saitejamandadi99>
2025-08-01 13:04:56 +0530

    changes made after testing with postman

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -1,7 +1,8 @@
 let appointments = []
-
+const doctors = require('../data/doctors');
 const bookAppointment = (req, res) =>{
     try {
+        console.log("Request Body:", req.body);
         const {name, doctorId, date} = req.body;
     if (!name || !doctorId || !date){
        return res.status(400).json({message:'All fields are required'}); //400 = bad request code
