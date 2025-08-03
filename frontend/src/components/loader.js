cc3e9d2bc08a6d9a8823663f4ea6511492ebf53b
saitejamandadi99 <saitejamandadi99>
2025-08-03 14:17:46 +0530

    added loader component

---
+++ frontend/src/components/loader.js
@@ -0,0 +1,14 @@
+// components/Loader.js
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center" style={{ height: '150px' }}>
+      <div className="spinner-border text-primary" role="status" style={{ width: '3rem', height: '3rem' }}>
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
